bcdc07643600e6d203b98ae6bcf7a2fe9882c8c7
Ayush Kumar Yadav <ayushyadav8056@gmail.com>
2024-02-03 17:35:04 +0530

    start first commit

---
+++ src/main/java/org/example/Swap.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Swap {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two number's ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = a;
+        a = b;
+        b = c;
+        System.out.println("Value of a is "+a);
+        System.out.println("Value of a is "+b);
+    }
+
+}
+
